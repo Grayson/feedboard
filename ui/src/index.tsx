3c2e1d1bc362d437afeab0f042f68708e3a8823f
Grayson Hansard <grayson.hansard@gmail.com>
2023-09-16 14:19:05 -0400

    Begin setting up routes

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -1,8 +1,17 @@
 import * as React from 'react';
 import * as ReactDOM from "react-dom";
+import {
+	createBrowserRouter,
+	RouterProvider
+} from 'react-router-dom';
 
-import App from './App';
 import "./styles.css";
+import RootPage from './routes/root';
+import Error from './error';
+
+const router = createBrowserRouter([
+	{ path: "/", element: <RootPage />, errorElement: <Error /> }
+])
 
 var mountNode = document.getElementById("app");
-ReactDOM.render(<App name="Jane" />, mountNode);
+ReactDOM.render(<React.StrictMode><RouterProvider router={router} /></React.StrictMode>, mountNode);
---
+++ ui/src/routes/root.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react'
+
+export default function RootPage() {
+	return <>
+		<h1>Feedboard</h1>
+	</>
+}

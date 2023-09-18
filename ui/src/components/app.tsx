3e714ea68532b19a2fbc78cc250c39c2aaa8d1e8
Grayson Hansard <grayson.hansard@gmail.com>
2023-09-18 12:44:27 -0400

    Link back to root page from header

--- ui/src/components/app.tsx
+++ ui/src/components/app.tsx
@@ -5,7 +5,7 @@ import DataRepoProvider from './datarepo'
 
 export default function App() {
 	return <>
-		<h1>Feedboard</h1>
+		<h1><a href='/'>Feedboard</a></h1>
 		<UserProvider>
 			<DataRepoProvider>
 				<Outlet />

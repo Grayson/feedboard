a6d2ec44e4ae51aea2e610e8cb3a76cd300eda44
Grayson Hansard <grayson.hansard@gmail.com>
2023-09-18 20:36:05 -0400

    Use RSS icons instead of `[Feed]` tags

--- ui/src/components/feedlist.tsx
+++ ui/src/components/feedlist.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import RssIcon from './rss-icon'
 
 export default function Feedlist({feeds}: {feeds: Feed[]}) {
 	return <div className='feed-list'>
@@ -10,6 +11,6 @@ export function FeedItem({feed}: {feed: Feed}) {
 	return <div className='item'>
 			<h3><a href={feed.url}>{feed.title}</a></h3>
 			<p>{feed.description}</p>
-			<p><a href={feed.url}>[Feed]</a></p>
+			<p><a href={feed.url}><RssIcon /></a></p>
 		</div>
 }
